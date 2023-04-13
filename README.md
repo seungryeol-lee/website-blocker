d80f3e0f9ddb21221fd0f94541b69388726d68e1
Seungryeol Lee <xyzsyz@gmail.com>
2023-04-13 23:00:27 +0800

    Added configuration

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Website Blocker
+
+Note:
+If running the script from windows:
+change the hosts path of the windows to --> 
+r"C:\Windows\System32\drivers\etc\hosts"
--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 # import tkinter
 import time
 from datetime import datetime
+import yaml
+
+with open("config.yaml") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
 
 # TODO: Create GUI using tkinter
 # time.sleep(1)
 
 # TODO: Specify the start time and end time
+# TODO: User defined input
+# TODO: If stop running, then terminate
 end_time = datetime(2023, 4, 12, 14)
 
-# TODO: Create a separate text document for store all the websites to block
 # TODO: When a new list is input: reset the current block and append the new ones only
-sites_to_block = []
 
-# TODO: Create separate variables for two hosts
-# hosts_path = "/etc/hosts"
-# hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
-hosts_path = "/mnt/c/Windows/System32/drivers/etc/hosts"
-redirect = "127.0.0.1"
+# From config.yaml
+sites_to_block = config["sites"]
+system = config["system"]
+hosts_path = config["hosts_path"][system]
+redirect = config["ip_local"]
+
 
 # TODO: Improve the algorithm and logic
 # TODO: Create a class object
 def block_sites():
     if datetime.now() < end_time:
-        print("block sites")
+        print("Blocking sites")
         with open(hosts_path, "r+") as hostsfile:
             hosts_content = hostsfile.read()
             for site in sites_to_block:
                 if site not in hosts_content:
                     hostsfile.write(redirect + " " + site + "\n")
     else:
-        print("unblock sites")
+        print("Unblocking sites")
         with open(hosts_path, "r+") as hostsfile:
             lines = hostsfile.readlines()
             hostsfile.seek(0)
