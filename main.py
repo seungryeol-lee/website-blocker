f1c6218b5e7c4e618cfe5e2241ea509c82f8dffd
Seungryeol Lee <xyzsyz@gmail.com>
2023-04-14 20:19:27 +0800

    Added user input

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ with open("config.yaml") as f:
 # TODO: Specify the start time and end time
 # TODO: User defined input
 # TODO: If stop running, then terminate
-end_time = datetime(2023, 4, 12, 14)
 
+# TODO: Add timer function instead of set time
 # TODO: When a new list is input: reset the current block and append the new ones only
 
 # From config.yaml
@@ -24,9 +24,29 @@ hosts_path = config["hosts_path"][system]
 redirect = config["ip_local"]
 
 
+# TODO: Rethink about the process
+
+
 # TODO: Improve the algorithm and logic
 # TODO: Create a class object
-def block_sites():
+def main():
+    while True:
+        cmd = input("Block/Unblock: ").lower()
+        if cmd == "block":
+            date_entry = input("YYYY-MM-DD-HH: ")
+            year, month, day, hour = map(int, date_entry.split("-"))
+            end_time = datetime(year, month, day, hour)
+            block_sites(end_time)
+            break
+        elif cmd == "unblock":
+            unblock_sites()
+            break
+        else:
+            print("Invalid input")
+            continue
+
+
+def block_sites(end_time):
     if datetime.now() < end_time:
         print("Blocking sites")
         with open(hosts_path, "r+") as hostsfile:
@@ -34,16 +54,18 @@ def block_sites():
             for site in sites_to_block:
                 if site not in hosts_content:
                     hostsfile.write(redirect + " " + site + "\n")
-    else:
-        print("Unblocking sites")
-        with open(hosts_path, "r+") as hostsfile:
-            lines = hostsfile.readlines()
-            hostsfile.seek(0)
-            for line in lines:
-                if not any(site in line for site in sites_to_block):
-                    hostsfile.write(line)
-            hostsfile.truncate()
+
+
+def unblock_sites():
+    print("Unblocking sites")
+    with open(hosts_path, "r+") as hostsfile:
+        lines = hostsfile.readlines()
+        hostsfile.seek(0)
+        for line in lines:
+            if not any(site in line for site in sites_to_block):
+                hostsfile.write(line)
+        hostsfile.truncate()
 
 
 if __name__ == "__main__":
-    block_sites()
+    main()
